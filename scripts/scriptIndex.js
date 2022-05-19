14698e123120ddf5bf560141cf726e2d58b66d8d
Secchi37 <secchibarbara85@gmail.com>
2022-05-19 12:39:17 +0200

    aggiunta base file

---
+++ scripts/scriptIndex.js
@@ -0,0 +1,60 @@
+let listaClienti = document.getElementById("listaClienti");
+let inserisciCliente = document.getElementById("inserisciCliente");
+let inserisciOrdine = document.getElementById("inserisciOrdine");
+let ottieniSingoloCliente = document.getElementById("ottieniSingoloCliente");
+let visualizzaOrdini = document.getElementById("visualizzaOrdini");
+
+let filtraCliente = document.getElementById("filtraCliente");
+let modificaCliente = document.getElementById("modificaCliente");
+let scaricaCliente = document.getElementById("scaricaCliente");
+let stampaCliente = document.getElementById("stampaCliente");
+let cercaCliente = document.getElementById("cercaCliente");
+let impostazioniClienti = document.getElementById("impostazioniClienti");
+let accediClienti = document.getElementById("accediClienti");
+
+let filtraOrdini = document.getElementById("filtraOrdini");
+let modificaOrdini = document.getElementById("modificaOrdini");
+let scaricaOrdini = document.getElementById("scaricaOrdini");
+let stampaOrdini=document.getElementById("stampaOrdini");
+let cercaOrdini = document.getElementById("cercaOrdini");
+let impostazioniOrdini = document.getElementById("impostazioniOrdini");
+let accediOrdini= document.getElementById("accediOrdini");
+
+
+
+let button = document.getElementById("button");
+let mycorpo = document.getElementById("mycorpo"); 
+
+/* mycorpo.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    alert("listaClienti.id");
+   
+}); */
+
+
+mycorpo.addEventListener("click",function (evt) {
+    evt.preventDefault();
+/* alert("listaClienti.id");
+alert("inserisciCliente.id");
+alert("inserisciOrdine.id");
+alert("inserisciOrdine.id");
+alert("ottieniSingoloCliente.id");
+alert("VisualizzaOrdini.id");
+
+alert("filtraCliente.id");
+alert("modificaCliente.id");
+alert("scaricaCliente.id");
+alert("cercaCliente.id");
+alert("stampaCliente.id");
+alert("impostazioniClienti.id");
+alert("accediClienti.id");
+
+alert("filtraOrdini.id");
+alert("modificaOrdini.id");
+alert("scaricaOrdini.id");
+alert("stampaOrdini.id");
+alert("cercaOrdini.id");
+alert("impostazioniOrdini.id");
+alert("accediOrdini.id"); */
+
+});
